e0ab9b5efb21cfe29fbda9e1b3065788d2086763
sauarv44-cpu <sauarv44@gmail.com>
2026-03-03 03:42:14 +0530

    Update Test4Application.java

--- src/main/java/com/test4/test4/Test4Application.java
+++ src/main/java/com/test4/test4/Test4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		int y =200;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
